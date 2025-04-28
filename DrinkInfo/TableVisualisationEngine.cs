bb66f14594c2ccd9cec6b66f2803de8608a7a89e
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-04-28 16:52:01 -0400

    showtable function

--- DrinkInfo/TableVisualisationEngine.cs
+++ DrinkInfo/TableVisualisationEngine.cs
@@ -1,12 +1,29 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
+using ConsoleTableExt;
 
 namespace DrinkInfo
 {
     public class TableVisualisationEngine
     {
-        
+        //                              Generic type list
+        public static void ShowTable<T>(List<T> tableData, [AllowNull] string tableName) where T : class
+        {
+            Console.Clear();
+            
+            if (tableName == null)
+                tableName = "";
+
+            Console.WriteLine("\n\n");
+
+            ConsoleTableBuilder
+            .From(tableData)
+            .WithColumn(tableName)
+            .ExportAndWrite();
+            Console.WriteLine("\n\n");
+        }
     }
 }
